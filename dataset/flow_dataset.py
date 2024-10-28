e32a0aaef4981590684c42ee6fd01525e0b51730
JuWanMaeng <aodwndhks@gmail.com>
2024-10-28 14:56:09 +0900

    flow dataset

--- dataset/flow_dataset.py
+++ dataset/flow_dataset.py
@@ -1,17 +1,17 @@
 from torch.utils import data as data
-
 import random
 import numpy as np
 import torch
 import cv2, os
 from ptlflow.utils import flow_utils
-from ptlflow.utils.flow_utils import flow_to_rgb
 
-class Flow_Blur_dataset(data.Dataset):
+
+class Flow_dataset(data.Dataset):
 
     def __init__(self, blur_txt_path, seed=None):
-        super(Flow_Blur_dataset, self).__init__()
+        super(Flow_dataset, self).__init__()
    
+        # blur path will change to flow path
         self.blur_txt_path = blur_txt_path
         self.paths = []
 
@@ -33,9 +33,6 @@ class Flow_Blur_dataset(data.Dataset):
         index = index % len(self.paths)
 
         blur_path = self.paths[index]        
-        blur_img = cv2.imread(blur_path)
-        blur_img = cv2.cvtColor(blur_img, cv2.COLOR_BGR2RGB)
-        blur_img = (blur_img - 127.5) / 127.5
 
         # Optical flow array
         flow_path = blur_path.replace('blur', 'flow/flows')
@@ -53,39 +50,31 @@ class Flow_Blur_dataset(data.Dataset):
         flow[nan_mask] = 0
         flow = np.clip(flow, -max_flow, max_flow)
 
-        ### 2d version ###
-        # flow_padded = np.pad(flow, ((0, 0), (0, 0), (0, 1)), mode='constant', constant_values=0)
-        # flow_rgb = flow_padded # 2d -> 3d with zero padding, actucally not rgb       
+        ### resize or random crop ###
+        flow_h, flow_w = flow.shape[0],flow.shape[1]
+        target_h, target_w = 768, 768
 
+        if flow_h > target_h and flow_w  > target_w:
+            flow = self.random_crop(flow, patch_size=[target_h,target_w])
 
-        ### rgb version ###
-        flow_rgb = flow_to_rgb(flow,flow_max_radius=150)  # 150 
-        flow_rgb = (flow_rgb - 127.5) / 127.5
-
-        # Random horizontal flipping
-        if np.random.rand() > 0.5:
-            blur_img = np.fliplr(blur_img).copy()
-            flow_rgb = np.fliplr(flow_rgb).copy()
-
-
-        blur_img_h, blur_img_w = blur_img.shape[0],blur_img.shape[1]
-        target_h, target_w = 480,640
-        # target_h, target_w = 540,960
-
-        if blur_img_h > target_h and blur_img_w  > target_w:
-            blur_img, flow_rgb = self.paired_random_crop(blur_img, flow_rgb, patch_size=[target_h,target_w])
-            # blur_img, flow_rgb = self.paired_random_crop(blur_img, flow_rgb, patch_size=[480,640])
         else:
-            blur_img = cv2.resize(blur_img,(target_w,target_h),interpolation=cv2.INTER_LANCZOS4)
-            flow_rgb = cv2.resize(flow_rgb,(target_w,target_h),interpolation=cv2.INTER_LANCZOS4)
+            flow = cv2.resize(flow, (target_w,target_h),interpolation=cv2.INTER_LANCZOS4)
+
+        ### augment flow ###
+        # Randomly select an augmentation command
+        commands = ['hflip', 'vflip' , 'noop']
+        command = np.random.choice(commands)
+        if command != 'noop':
+            flow = self.augment_flow(flow,command)
 
 
-        blur_tensor = torch.from_numpy(blur_img.transpose(2, 0, 1)).float()
-        flow_tensor = torch.from_numpy(flow_rgb.transpose(2, 0, 1)).float()
+        ### normalize_flow_to_tensor ###
+        n_flow = self.normalize_flow_to_tensor(flow)
+
+
+        flow_tensor = torch.from_numpy(n_flow).float()  # [u', v', r]
 
         return {
-            'blur': blur_tensor,
-            'lq_path': blur_path,
             'flow_path': flow_path,
             'flow': flow_tensor
         }
@@ -94,28 +83,19 @@ class Flow_Blur_dataset(data.Dataset):
         return len(self.paths)
 
 
-    def paired_random_crop(self,img_lqs, flow, patch_size):
+    def random_crop(self, flow, patch_size):
 
 
-        if not isinstance(img_lqs, list):
-            img_lqs = [img_lqs]
         if not isinstance(flow, list):
             flow = [flow]
 
 
-        h_lq, w_lq, _ = img_lqs[0].shape
-
+        h_lq, w_lq, _ = flow[0].shape
         new_h, new_w = patch_size[0], patch_size[1]
 
         # randomly choose top and left coordinates for lq patch
         top = random.randint(0, h_lq - new_h)
         left = random.randint(0, w_lq - new_w)
-
-        # crop lq patch
-        img_lqs = [
-            v[top:top + new_h, left:left + new_w, ...]
-            for v in img_lqs
-        ]
 
         # crop corresponding flow patch
 
@@ -124,15 +104,25 @@ class Flow_Blur_dataset(data.Dataset):
             for v in flow
         ]
 
-
-
-        if len(img_lqs) == 1:
-            img_lqs = img_lqs[0]
         if len(flow) == 1:
             flow = flow[0]
 
-        return img_lqs, flow
-    
+        return flow
+
+    def augment_flow(self, flow, command):
+
+        if command == 'hflip':  # horizontal
+            cv2.flip(flow, 1, flow)
+            flow[:, :, 0] *= -1
+
+        elif command ==  'vflip':  # vertical
+            cv2.flip(flow, 0, flow)
+            flow[:, :, 1] *= -1
+
+        else:
+            assert 'Wrong command!'
+
+        return flow
 
     def normalize_flow_to_tensor(self, flow):
         """
@@ -165,3 +155,17 @@ class Flow_Blur_dataset(data.Dataset):
         C = np.stack((x, y, z), axis=-1)
 
         return C
+    
+
+if __name__ == "__main__":
+    # Define the path to the text file with blur paths
+    blur_txt_path = "/workspace/Marigold/dataset/train/train_vae.txt"
+    dataset = Flow_dataset(blur_txt_path, seed=42)
+
+    # Test dataset length
+    print(f"Dataset length: {len(dataset)}")
+
+    # Test first sample
+    sample = dataset[0]
+    print(f"Sample flow path: {sample['flow_path']}")
+    print(f"Sample flow tensor shape: {sample['flow'].shape}")
